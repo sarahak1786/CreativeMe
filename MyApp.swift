e5cde16a96749ed6253a03ab9cf1133358de0a10
Sarah Akhtar <127272701+meta-oculus@users.noreply.github.com>
2023-03-19 14:11:29 -0700

    Add files via upload

---
+++ MyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyApp.swift
+//
+//
+//  Created by Sarah Akhtar on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CMNoteCreator()
+        }
+    }
+}
