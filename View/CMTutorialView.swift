2736f7f13b764b697bebcb728eff7a029fdc7edb
Sarah Akhtar <127272701+meta-oculus@users.noreply.github.com>
2023-04-02 16:01:03 -0700

    Add files via upload

---
+++ View/CMTutorialView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Sarah Akhtar on 4/2/23.
+//
+
+import SwiftUI
+
+struct CMTutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CMTutorialView_Previews: PreviewProvider {
+    static var previews: some View {
+        CMTutorialView()
+    }
+}
