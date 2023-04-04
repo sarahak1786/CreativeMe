199d7328e11f6048b81d22ba9d6a0d5caed9fc13
Sarah Akhtar <127272701+meta-oculus@users.noreply.github.com>
2023-04-03 21:47:34 -0700

    Add files via upload

--- View/Tutorials/Singles/CMHaikuView.swift
+++ View/Tutorials/Singles/CMHaikuView.swift
@@ -8,8 +8,154 @@
 import SwiftUI
 
 struct CMHaikuView: View {
+    
+    @Environment(\.colorScheme) var colorScheme
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.backgroundGradient
+                .ignoresSafeArea()
+            
+            GeometryReader { geo in
+                Rectangle()
+                    .foregroundColor(colorScheme == .dark ? Color.mainGray : .white)
+                    .cornerRadius(20)
+                    .shadow(radius: 20)
+                    .frame(width: geo.size.width * 0.90, height: geo.size.height * 0.95)
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY)
+                
+                Text("Learn to Write a Haiku")
+                    .font(.largeTitle)
+                    .fontWeight(.thin)
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.1)
+                
+                CMHaikuStructureView()
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY - geo.size.height * 0.26)
+                
+                CMHaikuFirstStepView()
+                    .frame(width: geo.size.width * 0.65, height: geo.size.height * 0.17)
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.40)
+                
+                CMHaikuSecondStepView()
+                    .frame(width: geo.size.width * 0.65, height: geo.size.height * 0.17)
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.59)
+                
+                CMHaikuThirdStepView()
+                    .frame(width: geo.size.width * 0.65, height: geo.size.height * 0.17)
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.78)
+                
+                Button {
+                    dismiss()
+                } label: {
+                    Label("", systemImage: "arrow.left")
+                        .font(.title)
+                }
+                .position(x: geo.frame(in: .local).minX + geo.size.width * 0.2, y: geo.frame(in: .local).minY + geo.size.height * 0.1)
+            }
+        }
+    }
+}
+
+struct CMHaikuStructureView: View {
+    var body: some View {
+        HStack {
+            VStack {
+                Text("5").foregroundColor(.mainPink)
+                Text("7").foregroundColor(.mainBlue)
+                Text("5").foregroundColor(.mainPink)
+            }
+            .font(.title.bold())
+                
+            VStack {
+                Rectangle()
+                    .frame(width: 50, height: 1)
+                    .padding(.bottom, 22)
+                
+                Rectangle()
+                    .frame(width: 50, height: 1)
+                    .padding(.bottom, 22)
+                
+                Rectangle()
+                    .frame(width: 50, height: 1)
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Watching the view glow,")
+                Text("The beach seems to ebb and flow;")
+                Text("Sifting the rowed sands")
+            }
+            .font(.title.bold())
+        }
+    }
+}
+
+struct CMHaikuFirstStepView: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.mainPink)
+                .cornerRadius(20)
+            
+            VStack(alignment: .leading) {
+                Text("Step #1")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                
+                Text(Information.shared.haikuInstructions[0])
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+            }
+            .padding()
+        }
+    }
+}
+
+struct CMHaikuSecondStepView: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.mainPink)
+                .cornerRadius(20)
+            
+            VStack(alignment: .leading) {
+                Text("Step #2")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                
+                Text(Information.shared.haikuInstructions[1])
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+            }
+            .padding()
+        }
+    }
+}
+
+struct CMHaikuThirdStepView: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.mainPink)
+                .cornerRadius(20)
+            
+            VStack(alignment: .leading) {
+                Text("Step #3")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                
+                Text(Information.shared.haikuInstructions[2])
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+            }
+            .padding()
+        }
     }
 }
 
--- View/Tutorials/Singles/CMPlayView.swift
+++ View/Tutorials/Singles/CMPlayView.swift
@@ -9,7 +9,199 @@ import SwiftUI
 
 struct CMPlayView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        CMPlayTutorialSlideOne()
+        //Add place to tap next
+    }
+}
+
+struct CMPlayTutorialSlideOne: View {
+    
+    @Environment(\.colorScheme) var colorScheme
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            Color.backgroundGradient
+                .ignoresSafeArea()
+            
+            GeometryReader { geo in
+                Rectangle()
+                    .foregroundColor(colorScheme == .dark ? Color.mainGray : .white)
+                    .cornerRadius(20)
+                    .shadow(radius: 20)
+                    .frame(width: geo.size.width * 0.90, height: geo.size.height * 0.95)
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY)
+                
+                Text("Learn to Write a Play")
+                    .font(.largeTitle)
+                    .fontWeight(.thin)
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.1)
+                
+                Image("PlayCharactersExample")
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.35)
+
+                CMPlayFirstStepView(step: "First")
+                    .frame(width: geo.size.width * 0.65, height: geo.size.height * 0.16)
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY + geo.size.height * 0.16)
+
+                CMPlaySecondStepView(step: "First")
+                    .frame(width: geo.size.width * 0.65, height: geo.size.height * 0.16)
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY + geo.size.height * 0.34)
+                
+                Button {
+                    dismiss()
+                } label: {
+                    Label("", systemImage: "arrow.left")
+                        .font(.title)
+                }
+                .position(x: geo.frame(in: .local).minX + geo.size.width * 0.2, y: geo.frame(in: .local).minY + geo.size.height * 0.1)
+            }
+        }
+    }
+}
+
+struct CMPlayTutorialSlideTwo: View {
+    
+    @Environment(\.colorScheme) var colorScheme
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            Color.backgroundGradient
+                .ignoresSafeArea()
+            
+            GeometryReader { geo in
+                Rectangle()
+                    .foregroundColor(colorScheme == .dark ? Color.mainGray : .white)
+                    .cornerRadius(20)
+                    .shadow(radius: 20)
+                    .frame(width: geo.size.width * 0.90, height: geo.size.height * 0.95)
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY)
+                
+                Text("Learn to Write a Play")
+                    .font(.largeTitle)
+                    .fontWeight(.thin)
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.1)
+                
+                Image("PlayDialogueExample")
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.25)
+
+                CMPlayFirstStepView(step: "Second")
+                    .frame(width: geo.size.width * 0.75, height: geo.size.height * 0.16)
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.47)
+
+                CMPlaySecondStepView(step: "Second")
+                    .frame(width: geo.size.width * 0.75, height: geo.size.height * 0.16)
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.65)
+
+                CMPlayThirdStepView()
+                    .frame(width: geo.size.width * 0.75, height: geo.size.height * 0.16)
+                    .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.83)
+                
+                Button {
+                    dismiss()
+                } label: {
+                    Label("", systemImage: "arrow.left")
+                        .font(.title)
+                }
+                .position(x: geo.frame(in: .local).minX + geo.size.width * 0.2, y: geo.frame(in: .local).minY + geo.size.height * 0.1)
+            }
+        }
+    }
+}
+
+struct CMPlayFirstStepView: View {
+    
+    let step: String
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.mainPink)
+                .cornerRadius(20)
+            
+            VStack(alignment: .leading) {
+                Text("Step #1")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                
+                if step == "First" {
+                    Text(Information.shared.playInstructions[0])
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                    
+                } else if step == "Second" {
+                    Text(Information.shared.playInstructions[2])
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                } else {
+                    Text("Something as gone wrong. Please try again.")
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct CMPlaySecondStepView: View {
+    
+    let step: String
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.mainPink)
+                .cornerRadius(20)
+            
+            VStack(alignment: .leading) {
+                Text("Step #2")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                
+                if step == "First" {
+                    Text(Information.shared.playInstructions[1])
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                    
+                } else if step == "Second" {
+                    Text(Information.shared.playInstructions[3])
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                } else {
+                    Text("Something as gone wrong. Please try again.")
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct CMPlayThirdStepView: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.mainPink)
+                .cornerRadius(20)
+            
+            VStack(alignment: .leading) {
+                Text("Step #3")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                
+                Text(Information.shared.playInstructions[4])
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+            }
+            .padding()
+        }
     }
 }
 
