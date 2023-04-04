da9c7561361a748c22def6b63d2b234fcd9a4442
Sarah Akhtar <127272701+meta-oculus@users.noreply.github.com>
2023-04-03 21:45:18 -0700

    Add files via upload

--- Model/CMInformation.swift
+++ Model/CMInformation.swift
@@ -34,11 +34,7 @@ class Information {
     let songInstructions: [String]
     let speechInstructions: [String]
     
-    let stepOption: [String]
-    let tipOption: [String]
-    let otherOption: [String]
-    
-    private init() {
+    init() {
         randomPoem = ["Write a poem about a well enjoyed trip to any destination of your choosing. Make this location a symbol of the poem.", "Write a poem that has a multi-layered symbol that ultimately shows the relationship between two chosen topics.", "Write a poem with as many literary devices as you can. There is no limit.", "Write a poem that has at least five metaphors and contains a consistent rhyme scheme.", "Write a poem that begins with a consistent rhyme scheme, but breaks at some instance to reveal a compelling message.", "Write a poem that introduces the nuances of a show, artistic piece, or phenomenon that interests you.", "Write a poem that creates an original Greek-like society with a moral message.", "Write a poem that has purposeful usage of syntax such as semicolons, line breaks, and enjambment.", "Write a poem that alludes to one of your favorite novels and evoke your opinion through the tone and/or diction.", "Write a poem about a painting of your choosing. The poem should be read alongside viewing the poem in reflection, reference, or analysis of the piece.", "Write a poem that is of 25+ lines with a style of flow that reveals the importance of its length."]
         
         randomHaiku = ["Write a haiku about a cat walking along the beach without using any of the words in this sentence.", "Write a haiku about a person close to you. Bonus points if you share this haiku with them!", "Write a haiku about a historical event that you find is important or interesting.", "Write a haiku that does not contain the letter ‘J’ in it.", "Write a haiku that rhymes at the end of each line.", "Write a haiku about your greatest passion; show this in your writing!", "Write a haiku about your day. Include one metaphor that aids your tone.", "Write a haiku that has a strongly Romantic tone.", "Write a haiku like a mad scientist on an adventure to Ireland."]
@@ -61,32 +57,26 @@ class Information {
         
         randomSpeech = ["Write a speech about how to conquer the world. Make it inspirational.", "Write a speech for the President. Recite the speech to someone else and have them rate your policy.", "Write a speech for the State of the Union Address. This speech is given every year in the United States and often concludes with, 'The state of the union is strong.'", "Write a speech about speeches. Incorporate the emotional connection you have to speeches, or lack thereof.", "Write a speech that is secretly about chickens. See how long it takes for someone to figure out what the speech is referring to.", "Write a speech that you feel you’d want to give in person. Compare this speech to others and improve on your writing.", "Write a speech that involves a strong opinion you have. Reflect on how the speech may have altered your opinion afterwards."]
         
-        poemInstructions = ["A poem is a form of writing that may have attributes like meter, stanzas, and rhyme schemes.", "In this poem, the rhyme scheme is ABAB-CC. A stanza is a group of lines of various lengths.", "While rhythm and stanzas are common, poems can be written in any style.", "Meter in poetry is used to determine what syllables of a poem are stressed or unstressed. When following a pattern, meter can create a desired effect.", "All unstressed syllables are are marked it a “U” shape above and stressed syllables are bolded. Popular meter patterns include iambic pentameter.", "Meter can provide structure and pattern for writing. Poetry may include specific meter patterns, but you are free to use any style or even make up your own!"]
+        poemInstructions = ["A poem is a form of writing that may have attributes like meter, stanzas, and rhyme schemes.", "In this poem, the rhyme scheme is ABAB-CC. A stanza is a group of lines of various lengths.", "While rhythm and stanzas are common, poems can be written in any style.", "Meter in poetry is used to determine what syllables of a poem are stressed or unstressed. When following a pattern, meter can create a desired effect.", "All unstressed syllables are are marked it a “U” shape above and stressed syllables are bolded. Popular meter patterns include iambic pentameter.", "Meter provides structure and pattern for writing. Poetry may include specific meter patterns, but you are free to use any style or even make up your own!"]
         
         haikuInstructions = ["A haiku is a 3-line poem with a 5-7-5 syllable structure. This type of poem is short and simple.","Syllables can be counted by a single vowel sound with any amount of consonants.","Haikus separate thoughts or create deliberate pauses (enjambment) for literary effect."]
         
-        aEssayInstructions = ["Your thesis statement should be between 1-2 sentences and creates a defensible (subjective) argument.", "Reasoning in your essay should be around twice the size of your evidence piece. Don’t be afraid to be extensive!"]
+        aEssayInstructions = ["Your thesis statement should be between 1-2 sentences and creates a defensible (subjective) argument.", "Reasoning in your essay should be around twice the size of your evidence piece. Don’t be afraid to be extensive!", "Body paragraphs should include two pieces of evidence and reasoning. This applies to the rebuttal as well."]
         
-        eEssayInstructions = ["An expository essay requires research prior to writing and makes an objective argument for the thesis.", "There is often 3-4 body paragraphs and does not include a rebuttal perspective. Expository essays are more data-driven."]
+        eEssayInstructions = ["An expository essay requires research prior to writing and makes an objective argument for the thesis.", "There is often 3-4 body paragraphs and does not include a rebuttal perspective. Expository essays are more data-driven.", "The introduction and conclusion are the same as other essays you may have encountered writing."]
         
-        nEssayInstructions = ["This essay type tells a story and mimics a plot structure. Exposition, climax, and resolution are all parts of a narrative.", "While narrative essays are stories, you may have to tell a personal narrative about an experience or journey you had."]
+        nEssayInstructions = ["This essay type tells a story and mimics a plot structure. Exposition, climax, and resolution are all parts of a narrative.", "While narrative essays are stories, you may have to tell a personal narrative about an experience or journey you had.", "Narratives are important for job interviews, college applications, and other personal aspects in your life."]
         
-        iEssayInstructions = ["Informative essays contain a thesis with three miniature arguments separated each by a comma.", "Each paragraph should expand upon one of the subsections in your thesis. Paragraphs should be assorted in order."]
+        iEssayInstructions = ["Informative essays contain a thesis with three miniature arguments separated each by a comma.", "Each paragraph should expand upon one of the subsections in your thesis. Paragraphs should be assorted in order.", "Paragraphs include three separate pieces of reasoning and evidence. Informative essays are extensive."]
         
         shakeSonnetInstructions = ["A Shakespearean sonnet is a 14-line poem that has a specific rhyme scheme of ABAB-CDCD-EFEF and EE. The poem contains iambic pentameter, which is shown above.", "A Shakespearean sonnet often has a “volta”, or a turn during the last two lines of the sonnet."]
         
         spenSonnetInstructions = ["A Spenserian sonnet contains one less rhyme couplet, but has two four-pairs sandwiched in the structure. The meter tends to follow iambic pentameter, but does not require it.", "Spenserian sonnets contain quatrains, but they are interlocked between the first three stanzas."]
         
-        playInstructions = ["To begin, a play contains a list of characters and age. Time and place are detailed as well.", "The title and authors of the play can be formatted in many manners, but must be centered.", "Create a title for  parts of the play as shown above. All stage directions are in parenthesis.", "Character names should be in all capital letters and dialogue falls directly below.", "Plays can be any length you’d like. Particularly, it is important to follow correct format."]
+        playInstructions = ["To begin, a play contains a list of characters and age. Time and place are detailed as well.", "The title and authors of the play can be formatted in many manners, but must be centered.", "Create a title for parts of the play as shown above. All stage directions are in parenthesis.", "Character names should be in all capital letters and dialogue falls directly below.", "Plays can be any length you’d like. Particularly, it is important to follow correct format."]
         
         songInstructions = ["A verse is a section of a song that contains different lyrics each time. These sections are important for building a purpose or story.", "A chorus is the main area of a song that is repeated and sticks with the listener. On each iteration, the chorus remains the same.", "A bridge is the most distinctive part of a song and connects two sections. It is often different from both the verse and chorus."]
         
         speechInstructions = ["Speeches often have a variety of purposes. Some involve governmental addresses while others are simply personal speeches.", "Most importantly, a listener should be able to get a main point from the speech. Focus on clearness and delivering coherently.", "The last words of a speech are the best opportunity to leave a lasting impression on the listener and everyone around."]
-        
-        stepOption = ["Tip #1:", "Tip #2:", "Tip #3:"]
-        
-        tipOption = ["Tip #1:", "Tip #2:", "Tip #3:"]
-        
-        otherOption = ["What is a verse?", "What is a chorus?", "What is a bridge?"]
     }
 }
