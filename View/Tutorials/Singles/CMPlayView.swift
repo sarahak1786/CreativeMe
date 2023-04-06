03444d5c5d8955c5e6892e0d772c879d5af81fef
Sarah Akhtar <127272701+meta-oculus@users.noreply.github.com>
2023-04-05 19:15:29 -0700

    Add files via upload

--- View/Tutorials/Singles/CMPlayView.swift
+++ View/Tutorials/Singles/CMPlayView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct CMPlayView: View {
     var body: some View {
         CMPlayTutorialSlideOne()
-        //Add place to tap next
     }
 }
 
@@ -18,6 +17,7 @@ struct CMPlayTutorialSlideOne: View {
     
     @Environment(\.colorScheme) var colorScheme
     @Environment(\.dismiss) var dismiss
+    @State private var showSecondSlide = false
     
     var body: some View {
         ZStack {
@@ -28,7 +28,7 @@ struct CMPlayTutorialSlideOne: View {
                 Rectangle()
                     .foregroundColor(colorScheme == .dark ? Color.mainGray : .white)
                     .cornerRadius(20)
-                    .shadow(radius: 20)
+//                    .shadow(radius: 5)
                     .frame(width: geo.size.width * 0.90, height: geo.size.height * 0.95)
                     .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY)
                 
@@ -40,11 +40,11 @@ struct CMPlayTutorialSlideOne: View {
                 Image("PlayCharactersExample")
                     .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.35)
 
-                CMPlayFirstStepView(step: "First")
+                CMPlayBodyView(initialWidth: 500, initialHeight: 100, finalWidth: 700, finalHeight: 200, index: 0)
                     .frame(width: geo.size.width * 0.65, height: geo.size.height * 0.16)
                     .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY + geo.size.height * 0.16)
 
-                CMPlaySecondStepView(step: "First")
+                CMPlayBodyView(initialWidth: 500, initialHeight: 100, finalWidth: 700, finalHeight: 200, index: 1)
                     .frame(width: geo.size.width * 0.65, height: geo.size.height * 0.16)
                     .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY + geo.size.height * 0.34)
                 
@@ -55,8 +55,18 @@ struct CMPlayTutorialSlideOne: View {
                         .font(.title)
                 }
                 .position(x: geo.frame(in: .local).minX + geo.size.width * 0.2, y: geo.frame(in: .local).minY + geo.size.height * 0.1)
+                
+                Button {
+                    showSecondSlide.toggle()
+                } label: {
+                    Label("", systemImage: "arrow.forward.circle.fill")
+                        .font(.title)
+                        .foregroundColor(.mainPink)
+                }
+                .position(x: geo.frame(in: .local).minX + geo.size.width * 0.8, y: geo.frame(in: .local).minY + geo.size.height * 0.1)
             }
         }
+        .fullScreenCover(isPresented: $showSecondSlide, content: CMPlayTutorialSlideTwo.init)
     }
 }
 
@@ -74,7 +84,7 @@ struct CMPlayTutorialSlideTwo: View {
                 Rectangle()
                     .foregroundColor(colorScheme == .dark ? Color.mainGray : .white)
                     .cornerRadius(20)
-                    .shadow(radius: 20)
+//                    .shadow(radius: 5)
                     .frame(width: geo.size.width * 0.90, height: geo.size.height * 0.95)
                     .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY)
                 
@@ -84,17 +94,20 @@ struct CMPlayTutorialSlideTwo: View {
                     .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.1)
                 
                 Image("PlayDialogueExample")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: geo.size.width * 0.80, height: geo.size.height * 0.95)
                     .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.25)
 
-                CMPlayFirstStepView(step: "Second")
+                CMPlayBodyView(initialWidth: 500, initialHeight: 100, finalWidth: 700, finalHeight: 200, index: 2)
                     .frame(width: geo.size.width * 0.75, height: geo.size.height * 0.16)
                     .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.47)
 
-                CMPlaySecondStepView(step: "Second")
+                CMPlayBodyView(initialWidth: 500, initialHeight: 100, finalWidth: 700, finalHeight: 200, index: 3)
                     .frame(width: geo.size.width * 0.75, height: geo.size.height * 0.16)
                     .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.65)
 
-                CMPlayThirdStepView()
+                CMPlayBodyView(initialWidth: 500, initialHeight: 100, finalWidth: 700, finalHeight: 200, index: 4)
                     .frame(width: geo.size.width * 0.75, height: geo.size.height * 0.16)
                     .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).minY + geo.size.height * 0.83)
                 
@@ -110,98 +123,106 @@ struct CMPlayTutorialSlideTwo: View {
     }
 }
 
-struct CMPlayFirstStepView: View {
+struct CMPlayBodyView: View {
     
-    let step: String
+    @State var flipped: Bool = false
+    @State var degrees: Double = 180.0
+    @State var width: CGFloat = 500
+    @State var height: CGFloat = 100
+    
+    let initialWidth: CGFloat
+    let initialHeight: CGFloat
+    let finalWidth: CGFloat
+    let finalHeight: CGFloat
+    
+    let index: Int
     
     var body: some View {
         ZStack {
-            Rectangle()
-                .foregroundColor(.mainPink)
-                .cornerRadius(20)
-            
-            VStack(alignment: .leading) {
-                Text("Step #1")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding()
-                
-                if step == "First" {
-                    Text(Information.shared.playInstructions[0])
-                        .font(.title)
-                        .foregroundColor(.white)
-                        .padding()
-                    
-                } else if step == "Second" {
-                    Text(Information.shared.playInstructions[2])
-                        .font(.title)
-                        .foregroundColor(.white)
-                        .padding()
-                } else {
-                    Text("Something as gone wrong. Please try again.")
+            if flipped {
+                CMPlayBack(width: self.$width, height: self.$height, index: index)
+            } else {
+                CMPlayFront(width: self.$width, height: self.$height)
+            }
+        }
+        .background(Color.gray)
+        .cornerRadius(20)
+        .rotation3DEffect(.degrees(degrees), axis: (x: 0, y: 1, z: 0))
+        .onTapGesture {
+            if self.flipped {
+                self.flipped = false
+                withAnimation {
+                    self.degrees += 180
+                    self.width = initialWidth
+                    self.height = initialHeight
+                }
+            } else {
+                self.flipped = true
+                withAnimation {
+                    self.degrees -= 180
+                    self.width = finalWidth
+                    self.height = finalHeight
                 }
             }
-            .padding()
         }
+        .onAppear(perform: setDimensions)
+    }
+    
+    func setDimensions() {
+        width = initialWidth
+        height = initialHeight
     }
 }
 
-struct CMPlaySecondStepView: View {
+struct CMPlayFront: View {
     
-    let step: String
+    @Binding var width: CGFloat
+    @Binding var height: CGFloat
     
     var body: some View {
-        ZStack {
-            Rectangle()
-                .foregroundColor(.mainPink)
-                .cornerRadius(20)
-            
-            VStack(alignment: .leading) {
-                Text("Step #2")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding()
-                
-                if step == "First" {
-                    Text(Information.shared.playInstructions[1])
-                        .font(.title)
-                        .foregroundColor(.white)
-                        .padding()
-                    
-                } else if step == "Second" {
-                    Text(Information.shared.playInstructions[3])
-                        .font(.title)
-                        .foregroundColor(.white)
-                        .padding()
-                } else {
-                    Text("Something as gone wrong. Please try again.")
-                }
-            }
-            .padding()
-        }
+        Rectangle()
+            .foregroundColor(Color.mainPink)
+            .frame(width: self.width, height: self.height)
+            .overlay(
+                Image(systemName: "doc.plaintext")
+                    .font(.largeTitle)
+            )
+            .foregroundColor(.white)
     }
 }
 
-struct CMPlayThirdStepView: View {
-    var body: some View {
-        ZStack {
-            Rectangle()
-                .foregroundColor(.mainPink)
-                .cornerRadius(20)
-            
-            VStack(alignment: .leading) {
-                Text("Step #3")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding()
-                
-                Text(Information.shared.playInstructions[4])
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding()
-            }
-            .padding()
+struct CMPlayBack: View {
+    
+    @Binding var width: CGFloat
+    @Binding var height: CGFloat
+    let index: Int
+    
+    var title: String {
+        if index == 0 {
+            return "Tip #1:"
+        } else if index == 1 {
+            return "Tip #2:"
+        } else if index == 2 {
+            return "Step #1:"
+        } else if index == 3 {
+            return "Step #2:"
+        } else if index == 4 {
+            return "Step #3:"
+        } else {
+            return ""
         }
+    }
+    
+    var body: some View {
+        Rectangle()
+            .foregroundColor(Color.mainPink)
+            .frame(width: self.width, height: self.height)
+            .overlay(
+                Text("\(title)\n\n\(Information.shared.playInstructions[index])")
+                    .padding(20)
+                    .font(.title)
+                    .foregroundColor(.white)
+            )
     }
 }
 
