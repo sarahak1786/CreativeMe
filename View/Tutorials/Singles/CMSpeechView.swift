44628d60c25d13e3c9bdbf80f66209adf9914a8b
Sarah Akhtar <127272701+meta-oculus@users.noreply.github.com>
2023-04-05 19:16:06 -0700

    Add files via upload

--- View/Tutorials/Singles/CMSpeechView.swift
+++ View/Tutorials/Singles/CMSpeechView.swift
@@ -21,7 +21,7 @@ struct CMSpeechView: View {
                 Rectangle()
                     .foregroundColor(colorScheme == .dark ? Color.mainGray : .white)
                     .cornerRadius(20)
-                    .shadow(radius: 20)
+//                    .shadow(radius: 5)
                     .frame(width: geo.size.width * 0.90, height: geo.size.height * 0.95)
                     .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY)
                 
@@ -33,15 +33,15 @@ struct CMSpeechView: View {
                 CMSpeechStructureView()
                     .position(x: geo.frame(in: .local).minX + geo.size.width * 0.29, y: geo.frame(in: .local).minY + geo.size.height * 0.35)
                 
-                CMSpeechFirstStepView()
+                CMSpeechBodyView(initialWidth: 200, initialHeight: 300, finalWidth: 340, finalHeight: 480, index: 0)
                     .frame(width: geo.size.width * 0.35, height: geo.size.height * 0.39)
                     .position(x: geo.frame(in: .local).minX + geo.size.width * 0.70, y: geo.frame(in: .local).minY + geo.size.height * 0.35)
 
-                CMSpeechSecondStepView()
+                CMSpeechBodyView(initialWidth: 200, initialHeight: 250, finalWidth: 340, finalHeight: 400, index: 1)
                     .frame(width: geo.size.width * 0.35, height: geo.size.height * 0.29)
                     .position(x: geo.frame(in: .local).minX + geo.size.width * 0.3, y: geo.frame(in: .local).midY + geo.size.height * 0.25)
 
-                CMSpeechThirdStepView()
+                CMSpeechBodyView(initialWidth: 200, initialHeight: 250, finalWidth: 340, finalHeight: 400, index: 2)
                     .frame(width: geo.size.width * 0.35, height: geo.size.height * 0.29)
                     .position(x: geo.frame(in: .local).maxX - geo.size.width * 0.3, y: geo.frame(in: .local).midY + geo.size.height * 0.25)
                 
@@ -105,75 +105,104 @@ struct CMSpeechStructureView: View {
     }
 }
 
-struct CMSpeechFirstStepView: View {
+struct CMSpeechBodyView: View {
+    
+    @State var flipped: Bool = false
+    @State var degrees: Double = 180.0
+    @State var width: CGFloat = 500
+    @State var height: CGFloat = 100
+    
+    let initialWidth: CGFloat
+    let initialHeight: CGFloat
+    let finalWidth: CGFloat
+    let finalHeight: CGFloat
+    
+    let index: Int
+    
     var body: some View {
         ZStack {
-            Rectangle()
-                .foregroundColor(.mainPink)
-                .cornerRadius(20)
-            
-            VStack(alignment: .leading) {
-                Text("Tip #1")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding()
-                
-                Text(Information.shared.speechInstructions[0])
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding()
+            if flipped {
+                CMSpeechBack(width: self.$width, height: self.$height, index: index)
+            } else {
+                CMSpeechFront(width: self.$width, height: self.$height)
             }
-            .padding()
         }
+        .background(Color.gray)
+        .cornerRadius(20)
+        .rotation3DEffect(.degrees(degrees), axis: (x: 0, y: 1, z: 0))
+        .onTapGesture {
+            if self.flipped {
+                self.flipped = false
+                withAnimation {
+                    self.degrees += 180
+                    self.width = initialWidth
+                    self.height = initialHeight
+                }
+            } else {
+                self.flipped = true
+                withAnimation {
+                    self.degrees -= 180
+                    self.width = finalWidth
+                    self.height = finalHeight
+                }
+            }
+        }
+        .onAppear(perform: setDimensions)
+    }
+    
+    func setDimensions() {
+        width = initialWidth
+        height = initialHeight
     }
 }
 
-struct CMSpeechSecondStepView: View {
+struct CMSpeechFront: View {
+    
+    @Binding var width: CGFloat
+    @Binding var height: CGFloat
+    
     var body: some View {
-        ZStack {
-            Rectangle()
-                .foregroundColor(.mainPink)
-                .cornerRadius(20)
-            
-            VStack(alignment: .leading) {
-                Text("Tip #2")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding()
-                
-                Text(Information.shared.speechInstructions[1])
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding()
-            }
-            .padding()
-        }
+        Rectangle()
+            .foregroundColor(Color.mainPink)
+            .frame(width: self.width, height: self.height)
+            .overlay(
+                Image(systemName: "doc.plaintext")
+                    .font(.largeTitle)
+            )
+            .foregroundColor(.white)
     }
 }
 
-struct CMSpeechThirdStepView: View {
-    var body: some View {
-        ZStack {
-            Rectangle()
-                .foregroundColor(.mainPink)
-                .cornerRadius(20)
-            
-            VStack(alignment: .leading) {
-                Text("Tip #3")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding()
-                
-                Text(Information.shared.speechInstructions[2])
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding()
-            }
-            .padding()
+struct CMSpeechBack: View {
+    
+    @Binding var width: CGFloat
+    @Binding var height: CGFloat
+    let index: Int
+    
+    var title: String {
+        if index == 0 {
+            return "Step #1:"
+        } else if index == 1 {
+            return "Step #2:"
+        } else if index == 2 {
+            return "Step #3:"
+        } else {
+            return ""
         }
     }
+    
+    var body: some View {
+        Rectangle()
+            .foregroundColor(Color.mainPink)
+            .frame(width: self.width, height: self.height)
+            .overlay(
+                Text("\(title)\n\n\(Information.shared.speechInstructions[index])")
+                    .padding(20)
+                    .font(.title)
+                    .foregroundColor(.white)
+            )
+    }
 }
-
 
 struct CMSpeechView_Previews: PreviewProvider {
     static var previews: some View {
