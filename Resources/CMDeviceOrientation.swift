14c04c6461bb1364a9a76d6612ee2dde7644bba8
Sarah Akhtar <127272701+meta-oculus@users.noreply.github.com>
2023-04-07 21:18:34 -0700

    Add files via upload

--- Resources/CMDeviceOrientation.swift
+++ Resources/CMDeviceOrientation.swift
@@ -2,25 +2,49 @@
 //  File.swift
 //  
 //
-//  Created by Sarah Akhtar on 3/19/23.
+//  Created by Sarah Akhtar on 4/2/23.
 //
 
 import SwiftUI
 
-struct DeviceRotationViewModifier: ViewModifier {
-    let action: (UIDeviceOrientation) -> Void
-
-    func body(content: Content) -> some View {
-        content
-            .onAppear()
-            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
-                action(UIDevice.current.orientation)
-            }
+//MARK: Class for Detecting Device Orientation
+final class OrientationInfo: ObservableObject {
+    
+    ///Variables for orientation and observing change
+    @Published var orientation: Orientation
+    private var _observer: NSObjectProtocol?
+    
+    ///Enumeration for types of device orientations
+    enum Orientation {
+        case portrait
+        case landscape
     }
-}
-
-extension View {
-    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
-        self.modifier(DeviceRotationViewModifier(action: action))
+    
+    ///Initializes whether device is in portrait or landscape
+    init() {
+        if UIDevice.current.orientation.isLandscape {
+            self.orientation = .landscape
+        } else {
+            self.orientation = .portrait
+        }
+        
+        _observer = NotificationCenter.default.addObserver(forName: UIDevice.orientationDidChangeNotification, object: nil, queue: nil) { [unowned self] input in
+            guard let givenDevice = input.object as? UIDevice else {
+                return
+            }
+            if givenDevice.orientation.isPortrait {
+                self.orientation = .portrait
+            }
+            else if givenDevice.orientation.isLandscape {
+                self.orientation = .landscape
+            }
+        }
+    }
+    
+    ///De-initializes the obeserver
+    deinit {
+        if let currentObserver = _observer {
+            NotificationCenter.default.removeObserver(currentObserver)
+        }
     }
 }
