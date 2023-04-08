5c7db1b77dcddc3280f0f8b6bba600cf7360f922
Sarah Akhtar <127272701+meta-oculus@users.noreply.github.com>
2023-04-07 21:20:03 -0700

    Add files via upload

--- Colors/CMColorExtension.swift
+++ Colors/CMColorExtension.swift
@@ -2,27 +2,30 @@
 //  ColorExtension.swift
 //  
 //
-//  Created by Sarah Akhtar on 3/18/23.
+//  Created by Sarah Akhtar on 4/2/23.
 //
 
 import SwiftUI
 
-//MARK: Added Colors
+//MARK: Custom Application Colors
 extension Color {
+    
+    ///Globally used colors for buttons, icons, and more
     static let mainBlue = Color("BKBlue")
     static let mainPink = Color("BKPink")
     static let mainGray = Color("BKGray")
     
+    ///Globally used dark versioned color for buttons, icons, and more
     static let darkBlue = Color("DarkBlue")
     static let darkPink = Color("DarkPink")
     static let darkPurple = Color("DarkPurple")
     
+    ///Globally used colors for main icon bar colors (named after each)
     static let purpleLearn = Color("PurpleLearn")
     static let purpleRandom = Color("PurpleRandom")
     static let purpleSave = Color("PurpleSave")
     static let blueButton = Color("BlueButton")
     
+    ///Main background gradient color for all views
     static let backgroundGradient =  LinearGradient(gradient: Gradient(colors: [mainBlue, mainPink]), startPoint: .top, endPoint: .bottom)
-    
-    static let reverseGradient =  LinearGradient(gradient: Gradient(colors: [mainPink, mainBlue]), startPoint: .top, endPoint: .bottom)
 }
